a7a6448207435dfc3653bfe8adce947d492d3d9a
Nicolae Cudlenco <nicolae.cudlenco@cti.pub.ro>
2026-04-19 21:55:11 +0200

    Expose multi-modal capture to Lua
    New CLuaMultiModalDefs module registering:
      - captureMultiModalFrame(rgbPath, segPath, depthPath, saveRgbToVideo,
        saveSegToVideo, saveDepthToVideo, jpegQuality)
      - startVideoRecording(modalityId, path, width, height, fps, bitrate)
      - stopVideoRecording(modalityId)
      - writeMultiModalMapping(path)
      - setMultiModalSegmentation(enabled)
      - setCleanCaptureMode(enabled) -- mutes MTA's overlay/HUD/tonemap
        pipeline so the captured backbuffer matches the pre-composite pixels.

---
+++ Client/mods/deathmatch/logic/luadefs/CLuaMultiModalDefs.h
@@ -0,0 +1,25 @@
+/*****************************************************************************
+ *
+ *  PROJECT:     Multi Theft Auto
+ *  LICENSE:     See LICENSE in the top level directory
+ *  FILE:        Client/mods/deathmatch/logic/luadefs/CLuaMultiModalDefs.h
+ *
+ *  Multi Theft Auto is available from https://multitheftauto.com/
+ *
+ *****************************************************************************/
+
+#pragma once
+#include "CLuaDefs.h"
+
+class CLuaMultiModalDefs : public CLuaDefs
+{
+public:
+    static void LoadFunctions();
+
+    LUA_DECLARE(CaptureMultiModalFrame);
+    LUA_DECLARE(StartVideoRecording);
+    LUA_DECLARE(StopVideoRecording);
+    LUA_DECLARE(WriteMultiModalMapping);
+    LUA_DECLARE(SetMultiModalSegmentation);
+    LUA_DECLARE(SetCleanCaptureMode);
+};
